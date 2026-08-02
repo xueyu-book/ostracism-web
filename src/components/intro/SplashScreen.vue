e0aa688fb29686c49b7412f30926d0d029fc17bf
廖庚裕 <19876106057@139.com>
2026-08-02 17:56:35 +0800

    feat: 兼容手机底部安全区域

--- src/components/intro/SplashScreen.vue
+++ src/components/intro/SplashScreen.vue
@@ -216,6 +216,10 @@ $slide-distance: 56px;
     margin: 0 auto;
     width: 152px;
     animation: splash-fade-in 0.7s $stage-ease $stage-5-delay both;
+
+    @media (max-width: 768px) {
+      bottom: 200px;
+    }
   }
 }
 </style>
